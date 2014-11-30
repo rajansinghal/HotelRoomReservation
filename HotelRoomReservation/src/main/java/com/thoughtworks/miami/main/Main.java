11cdd191044780bcf12a1b1e7b07d3834fcfccda
rajansinghal <mr.rajansinghal@gmail.com>
2014-11-30 16:47:14 +0530

    New change

--- HotelRoomReservation/src/main/java/com/thoughtworks/miami/main/Main.java
+++ HotelRoomReservation/src/main/java/com/thoughtworks/miami/main/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		ServiceManager.getOnlinereservationservice().findCheapestHotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)");
-		System.out.println(ServiceManager.getOnlinereservationservice().findCheapestHotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"));
-		System.out.println(ServiceManager.getOnlinereservationservice().findCheapestHotel("Regular: 22Mar2009(fri), 09Mar2009(sat), 11Mar2009(sun)"));
+		System.out.println(ServiceManager.getOnlinereservationservice().findCheapestHotel("Regular: 30Mar2009(fri), 19Mar2009(sat), 13Mar2009(sun)"));
+		System.out.println(ServiceManager.getOnlinereservationservice().findCheapestHotel("Regular: 03Mar2009(fri), 21Mar2009(sat), 14Mar2009(sun)"));
 	
 
 	}
